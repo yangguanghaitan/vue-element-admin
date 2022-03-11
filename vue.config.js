6d3125f4ba4648308dc10990d90f0529ee1d0814
yangguanghaitan <2898666969@qq.com>
2022-03-11 14:10:07 +0800

    断点调试用的

--- vue.config.js
+++ vue.config.js
@@ -39,6 +39,7 @@ module.exports = {
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
+    devtool: 'source-map',
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     name: name,
